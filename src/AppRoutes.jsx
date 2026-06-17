f530e43d5166872660a808d58c390bb7a7c72e14
rixaapplication <julianbruch87@gmail.com>
2026-06-17 16:17:10 -0300

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './apps/comercio/Dashboard';
 
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/comercio" element={<Dashboard />} />
+      {/* Aquí podés agregar más rutas para otras secciones */}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
